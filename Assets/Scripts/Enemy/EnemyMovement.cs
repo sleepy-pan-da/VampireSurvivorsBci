94f4d9dde1dcda1b0c3929d764f5ba6ddbc8dbc4
sleepy-pan-da <61502188+sleepy-pan-da@users.noreply.github.com>
2023-01-10 15:24:34 +0800

    Add enemy animation, sprite will flip based on direction towards player

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -10,17 +10,23 @@ public class EnemyMovement : MonoBehaviour
     private int speed;
     private Rigidbody2D rb;
     private KnockbackManager knockbackManager;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
     private void Start()
     {
         playerTransform = GameObject.FindWithTag("Player")?.transform; 
         speed = GetComponent<EnemyStatsManager>().Stats.MovementSpeed;
 
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
         rb = GetComponent<Rigidbody2D>();
         knockbackManager = GetComponent<KnockbackManager>();
 
         knockbackManager.OnBegin += () => {enabled = false;};
         knockbackManager.OnDone += () => {enabled = true;};
+
     }
 
     private void FixedUpdate()
@@ -30,9 +36,16 @@ public class EnemyMovement : MonoBehaviour
 
     private void FollowPlayer()
     {
-        if (playerTransform == null) return;
+        if (playerTransform == null) 
+        {
+            animator.SetBool("PlayerIsDead", true);
+            return;
+        }
         float step = speed * Time.deltaTime;
         Vector2 desiredPosition = Vector2.MoveTowards(transform.position, playerTransform.position, step);
         rb.MovePosition(desiredPosition);
+
+        Vector2 directionTowardsPlayer = (playerTransform.position - transform.position).normalized;
+        spriteRenderer.flipX = directionTowardsPlayer.x < 0;
     }
 }
